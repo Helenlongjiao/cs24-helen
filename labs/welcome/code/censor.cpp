4a34d1cff505f14d82dd0c63433d2c26c1fc71d0
HelenLongjiao <longjiao@ucsb.edu>
2024-04-03 20:45:45 -0700

    change int to size_t

--- labs/welcome/code/censor.cpp
+++ labs/welcome/code/censor.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 
-int main(size_t argc, char* argv[]){
+int main(int argc, char* argv[]){
     if(argc != 2){
         std::cout<<"USAGE: censor [length]";
     }
