2b866f911eb7a29035eced702840af1961b9b9bb
HelenLongjiao <longjiao@ucsb.edu>
2024-04-03 21:20:37 -0700

    change int to size_t

--- labs/welcome/code/censor.cpp
+++ labs/welcome/code/censor.cpp
@@ -4,15 +4,18 @@
 int main(int argc, char* argv[]){
     if(argc != 2){
         std::cout<<"USAGE: censor [length]";
+        return 1;
     }
     std::string input;
     std::getline(std::cin, input);
     size_t count1 = 0;
     size_t count2 = 0;
-    while(count2 < input.size()){
+    size_t first = 0;
+    while(count2 <= input.size()){
         while(input[count1] == ' '){
             count1 ++;
             count2 = count1;
+            first = count1;
         }
         if(input[count2] != ' ' && count2 < input.size()){
             count2 ++;
@@ -20,7 +23,7 @@ int main(int argc, char* argv[]){
         else{
             int diff = count2 - count1;
             if(diff != std::stoi(argv[1])){
-                if(count1 != 0){
+                if(count1 != first){
                     std::cout<<' ';
                 }
                 for(size_t i = count1; i < count2; ++i){
